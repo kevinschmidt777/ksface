b834d3bd77caaec5ac29cdfddef58a987627c613
Kevin Schmidt <kevin.schmidt@ewry.de>
2019-04-23 14:04:49 +0200

    Grid Responsive und dynamischer GAP.

--- src/js/grid.js
+++ src/js/grid.js
@@ -3,10 +3,35 @@
 
 // Select parent from .ks-grid to apply overflow:hidden (hide overflowing grid div)
 document.addEventListener('DOMContentLoaded', function(event) {
-  const grids = document.getElementsByClassName('ks-grid');
+  const grids1 = document.getElementsByClassName('ks-grid');
+  const grids2 = document.getElementsByClassName('ks-grid-minimal');
+  const grids3 = document.getElementsByClassName('ks-grid-small');
+  const grids4 = document.getElementsByClassName('ks-grid-medium');
+  const grids5 = document.getElementsByClassName('ks-grid-large');
+  const grids6 = document.getElementsByClassName('ks-grid-ultra');
+  const grids = [];
   const gridParents = [];
 
-  Array.prototype.forEach.call(grids, (grid) => {
+  Array.prototype.forEach.call(grids1, (grid) => {
+    grids.push(grid);
+  });
+  Array.prototype.forEach.call(grids2, (grid) => {
+    grids.push(grid);
+  });
+  Array.prototype.forEach.call(grids3, (grid) => {
+    grids.push(grid);
+  });
+  Array.prototype.forEach.call(grids4, (grid) => {
+    grids.push(grid);
+  });
+  Array.prototype.forEach.call(grids5, (grid) => {
+    grids.push(grid);
+  });
+  Array.prototype.forEach.call(grids6, (grid) => {
+    grids.push(grid);
+  });
+
+  grids.forEach((grid) => {
     gridParents.push(grid.parentElement);
   });
 
