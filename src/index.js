0621016d4f75bda1e423d081a6175ef7af61ab61
Kevin <kevin@kswebentwicklung.de>
2019-09-27 07:21:25 +0200

    Expanded spacing with top, right, bottom, left appliers for padding and margin.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 // KSFace - www.ksface.com
-// Copyright (c) 2019 Kevin Schmidt Webentwicklung - www.kswebentwicklung.de
+// Copyright (c) 2019 Kevin Schmidt - www.kswebentwicklung.de
 
-import './js/icons';
 import './js/grid';
